c5f08eac83cb483899e38b8f99302cd9e91702a3
Federico García <garciafedericoagustin@gmail.com>
2021-06-03 11:16:07 -0300

    test_ExtractChannelArrays_1_channel()

--- tests/analysis/test_pcm.py
+++ tests/analysis/test_pcm.py
@@ -2,6 +2,14 @@ from pyspek.analysis.pcm import *
 import numpy as np
 import miniaudio as audio
 
+def test_ExtractChannelArrays_1_channel():
+    samples = audio.array.array("b", [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+    audio_file = audio.DecodedSoundFile("test", 1, 48000, audio.SampleFormat.SIGNED16, samples)
+
+    channel_1 = np.array([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+
+    assert np.testing.assert_equal(ExtractChannelArray(1, audio_file).astype(int), channel_1) is None
+
 def test_ExtractChannelArrays_2_channels():
     samples = audio.array.array("b", [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
     audio_file = audio.DecodedSoundFile("test", 2, 48000, audio.SampleFormat.SIGNED16, samples)
