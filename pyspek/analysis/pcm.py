c268fd50f44872158c1bb9c4dbabcd080367244e
Federico García <garciafedericoagustin@gmail.com>
2021-06-03 10:35:23 -0300

    test_ExtractChannelArrays_2_channels()

--- pyspek/analysis/pcm.py
+++ pyspek/analysis/pcm.py
@@ -8,17 +8,19 @@ def ExtractChannelArray(channel: int, DecodedSoundFile: audio.DecodedSoundFile) 
         raise Exception("The number of the selected channel exceeds the existing number of channels.")
         
     samples_per_channel = DecodedSoundFile.num_frames
+    number_of_channels = DecodedSoundFile.nchannels
     channel_array = np.zeros(samples_per_channel)
     index = 0
-    for sample in range(channel - 1, samples_per_channel, channel):
+    for sample in range(channel - 1, samples_per_channel*number_of_channels, number_of_channels):
         channel_array[index] = DecodedSoundFile.samples[sample]
         index = index + 1
     return channel_array
 
 def GenerateTimeArray(DecodedSoundFile: audio.DecodedSoundFile) -> np.ndarray:
-    num_frames = DecodedSoundFile.num_frames
+    samples_per_channel = DecodedSoundFile.num_frames
     sample_rate = DecodedSoundFile.sample_rate
-    time_array = np.zeros(num_frames)
-    for index in range(num_frames):
+    time_array = np.zeros(samples_per_channel)
+    for index in range(samples_per_channel):
         time_array[index] = index / sample_rate # in seconds
     return time_array
+
---
+++ tests/analysis/test_pcm.py
@@ -0,0 +1,13 @@
+from pyspek.analysis.pcm import *
+import numpy as np
+import miniaudio as audio
+
+def test_ExtractChannelArrays_2_channels():
+    samples = audio.array.array("b", [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+    audio_file = audio.DecodedSoundFile("test", 2, 48000, audio.SampleFormat.SIGNED16, samples)
+
+    channel_1 = np.array([-10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10])
+    channel_2 = np.array([-9, -7, -5, -3, -1, 1, 3, 5, 7, 9, 11])
+
+    assert np.testing.assert_equal(ExtractChannelArray(1, audio_file).astype(int), channel_1) is None
+    assert np.testing.assert_equal(ExtractChannelArray(2, audio_file).astype(int), channel_2) is None
