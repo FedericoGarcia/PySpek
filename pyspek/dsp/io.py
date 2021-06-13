0d415be1961104f76ba9f19dcc3fa6287bfa806a
Federico García <garciafedericoagustin@gmail.com>
2021-06-13 03:55:12 -0300

    Update io.py

--- pyspek/dsp/io.py
+++ pyspek/dsp/io.py
@@ -2,21 +2,6 @@ import miniaudio
 import numpy
 import pyspek.dsp.core.audio
 
-def ExtractChannelArray(channel: int, DecodedSoundFile: miniaudio.DecodedSoundFile) -> numpy.ndarray:
-    if channel < 1:
-        raise Exception("The selected channel number must be 1 or greater.")
-    if channel > DecodedSoundFile.nchannels:
-        raise Exception("The number of the selected channel exceeds the existing number of channels.")
-        
-    samples_per_channel = DecodedSoundFile.num_frames
-    number_of_channels = DecodedSoundFile.nchannels
-    channel_array = numpy.zeros(samples_per_channel)
-    index = 0
-    for sample in range(channel - 1, samples_per_channel*number_of_channels, number_of_channels):
-        channel_array[index] = DecodedSoundFile.samples[sample]
-        index = index + 1
-    return channel_array
-
 def decode_file(audio_file_path = str) -> pyspek.dsp.core.audio.Data:
     audio_file = miniaudio.decode_file(audio_file_path)
     pcm_samples = audio_file.samples
@@ -27,23 +12,8 @@ def decode_file(audio_file_path = str) -> pyspek.dsp.core.audio.Data:
 
     samples = numpy.zeros(shape = (channels_count, channel_samples_count))
     
-    for sample in pcm_samples:
-        for channel in samples
-        samples[channel][sample] = 
+    for sample in range(channel_samples_count):
+        for channel in range(channels_count):
+            samples[channel][sample] = pcm_samples[channel*sample]
 
     return pyspek.dsp.core.audio.Data(bit_depth, sample_rate, samples)
-
-
-def GenerateTimeArray(DecodedSoundFile: miniaudio.DecodedSoundFile) -> numpy.ndarray:
-    samples_per_channel = DecodedSoundFile.num_frames
-    sample_rate = DecodedSoundFile.sample_rate
-    time_array = numpy.zeros(samples_per_channel)
-    for index in range(samples_per_channel):
-        time_array[index] = index / sample_rate
-    return time_array
-
-
-
-
-
-
